d0b05734ce09e71b6324cbb8fbe685fe5a552255
hokiebrian <64511748+hokiebrian@users.noreply.github.com>
2023-06-05 00:13:42 -0400

    Additional selects foor seconary

--- custom_components/openai_music/select.py
+++ custom_components/openai_music/select.py
@@ -14,6 +14,7 @@ async def async_setup_entry(hass, config, async_add_entities):
             InfoSelectEntity(),
             InfoSelectEntitySecondary(),
             PersonalitySelectEntity(),
+            PersonalitySelectEntitySecondary(),
             ImageSelectEntity(),
         ]
     )
@@ -102,6 +103,32 @@ class PersonalitySelectEntity(SelectEntity):
             self._state = option
             self.async_write_ha_state()
 
+class PersonalitySelectEntitySecondary(SelectEntity):
+    """Select OpenAI Personality."""
+
+    _attr_icon = "mdi:tooltip-account"
+    _attr_unique_id = "OAIPersonalitySecondary"
+
+    def __init__(self):
+        self._state = "Normal"
+        self._attr_options = sorted([key for key, value in PERSONALITIES.items()])
+
+    @property
+    def name(self):
+        return "OpenAI Music Personality Profile (Secondary)"
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def current_option(self):
+        return None
+
+    async def async_select_option(self, option):
+        if option in self._attr_options:
+            self._state = option
+            self.async_write_ha_state()
 
 class ImageSelectEntity(SelectEntity):
     """Select Image Creation Profile."""
